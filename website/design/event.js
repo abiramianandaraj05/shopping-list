d13b082ec71373f6bdc4555bfa71845381867d29
psyaa35 <psyaa35@exmail.nottingham.ac.uk>
2025-08-30 17:59:42 +0100

    changes

--- website/design/event.js
+++ website/design/event.js
@@ -1,8 +1,4 @@
-//import { name } from 'ejs';
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://eylcglryevhfjsltgtvg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bGNnbHJ5ZXZoZmpzbHRndHZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0Njg1NjYsImV4cCI6MjA2OTA0NDU2Nn0.Hg6gDZDy_lxHhWZH-XqWYlRDeb_Dg-cij-c1tgpSnNs'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
